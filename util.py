e8464f4722d4718a5d333c50bd10c271eb013a42
Rainbowcat13 <rainbowcat1308@gmail.com>
2025-02-18 22:19:50 +0300

    Add alogrithm for scanning combinations of small variable counts and sorting them in order of outputted variables count

--- util.py
+++ util.py
@@ -1,3 +1,9 @@
+from pysat.formula import CNF
+from pysat.solvers import Glucose3
+
+from itertools import combinations
+
+
 def fullscan_values(assumption, set_size):
     assumption = list(assumption)
     for n in range(2 ** set_size):
@@ -6,3 +12,23 @@ def fullscan_values(assumption, set_size):
 
 def assumption_key(assumption):
     return list(map(abs, assumption))
+
+
+def precount_set_order(formula: CNF, solver: Glucose3, level=2):
+    assumptions = list(combinations(list(range(1, formula.nv + 1)), level))
+    assumptions_count = len(assumptions)
+    outputs = [0] * assumptions_count
+    for index in range(assumptions_count):
+        for assumption_with_signs in fullscan_values(assumptions[index], level):
+            no_conflicts, result = solver.propagate(assumption_with_signs)
+            outputs[index] += len(result)
+
+    assumptions_with_result_powers = list(zip(assumptions, outputs))
+    assumptions_with_result_powers.sort(key=lambda item: -item[1])
+    return [item[0] for item in assumptions_with_result_powers]
+
+
+if __name__ == '__main__':
+    formula = CNF(from_file='formula.cnf')
+    g = Glucose3(formula.clauses)
+    print(precount_set_order(formula, g, level=3))
