74c0f946d2e99d94f25e6aa8829fa60c84d07de0
danya <d.golov@vkteam.ru>
2025-03-03 20:14:08 +0300

    Polynomial graph search (not working) & tools for proper unit propagation

--- graph_search.py
+++ graph_search.py
@@ -1,48 +1,93 @@
+import heapq
+
 from pysat.formula import CNF
 from pysat.solvers import Glucose3
 
-import heapq
+from util import assumption_key
+
+# Идея не работает и непонятно как сделать чтобы работала
 
 
 def find_start_point(formula: CNF, solver: Glucose3):
     start_point = None
     output = None
     for var in range(1, formula.nv + 1):
-        conflict_pos, output_pos = solver.propagate([var])
-        conflict_neg, output_neg = solver.propagate([-var])
-        if conflict_pos or conflict_neg:
+        no_conflict_pos, output_pos = solver.propagate([var])
+        no_conflict_neg, output_neg = solver.propagate([-var])
+        if not no_conflict_pos or not no_conflict_neg:
             continue
-        output_sum = set(output_pos).intersection(set(output_neg))
-        if len(output_sum) > len(output):
-            output = output_sum
+        output_pos = set(assumption_key(output_pos))
+        output_neg = set(assumption_key(output_neg))
+        output_sum = output_pos.intersection(output_neg)
+        if output is None or len(output_sum) > len(output[0].intersection(output[1])):
+            output = (output_pos, output_neg)
             start_point = var
 
     return start_point, output
 
 
-def dijkstra_with_border(formula: CNF, solver: Glucose3, start_var: int, start_output: set[int], border: int):
+def dijkstra_with_border(
+        formula: CNF,
+        solver: Glucose3,
+        start_var: int,
+        start_output: tuple[set[int], set[int]],
+        border: int
+) -> list[tuple[int, list[int], set[int]]]:
     q = []
-    dist = [(-1, 1, {i}) for i in range(formula.nv + 1)]
-    dist[start_var] = (-len(start_output), 1, start_output)
-    heapq.heappush(q, (-len(start_output), [start_var], start_output))
+    dist = [(-1, [], {i}) for i in range(2 * formula.nv + 1)]
+    dist[start_var] = (-len(start_output[0]), [start_var], start_output[0])
+    dist[-start_var] = (-len(start_output[1]), [-start_var], start_output[1])
+    heapq.heappush(q, (-len(start_output[0]), [start_var], start_output[0]))
+    heapq.heappush(q, (-len(start_output[1]), [-start_var], start_output[1]))
+
     while q:
         power, variables, output = heapq.heappop(q)
         power *= -1
+        if len(variables) == border:
+            break
         # нужен множитель [-1, 1]
         # пропагейтить с известными -/+ и в стартвар заложить
-        for v in range(1, formula.nv + 1):
+        for v in range(-formula.nv, formula.nv + 1):
+            if v == 0:
+                continue
 
-            power_v, length_v, output_v = dist[v]
+            power_v, vars_v, output_v = dist[v]
+            power_v *= -1
             variables.append(v)
-            conflict, new_output = solver.propagate(variables)
+            no_conflicts, new_output = solver.propagate(variables)
+            if not no_conflicts:
+                variables.pop()
+                continue
 
+            if power_v < len(new_output):
+                dist[v] = (-len(new_output), variables.copy(), new_output.copy())
+                heapq.heappush(q, (-len(new_output), variables.copy(), new_output.copy()))
+
+            variables.pop()
+
+    return dist
 
 
 f = CNF(from_file='formula.cnf')
 g = Glucose3(f.clauses)
-start = find_start_point(f, g)
-if start is None:
+start_pt, start_vars = find_start_point(f, g)
+if start_pt is None:
     print('Not satisfiable at all')
     exit(1)
 
+d = dijkstra_with_border(f, g, start_pt, start_vars, 4)
+max_output = None
+result_set = None
+for pwr, vrs, out in d:
+    max_output = max_output or out
+    result = result_set or vrs
+    if pwr > len(max_output):
+        max_output = out
+        result_set = vrs
 
+print('Output:')
+print(len(max_output))
+print(*max_output)
+print('Variables set:')
+print(len(result_set))
+print(*result_set)
---
+++ variables.py
@@ -0,0 +1,66 @@
+from pysat.formula import CNF
+
+
+def unit_propagation(formula: CNF) -> (bool, CNF):
+    new_formula = CNF(from_clauses=formula.clauses)
+    for variable in range(1, formula.nv + 1):
+        alone_pos = False
+        alone_neg = False
+        pos = set()
+        neg = set()
+        neutral = set()
+        clauses = formula.clauses.copy()
+        for index, clause in enumerate(clauses):
+            if clause == [variable]:
+                pos.add(index)
+                alone_pos = True
+            elif clause == -variable:
+                neg.add(index)
+                alone_neg = True
+            elif variable in clause:
+                pos.add(index)
+            elif -variable in clause:
+                neg.add(index)
+            else:
+                neutral.add(index)
+
+        result_clauses = clauses.copy()
+        if alone_neg and alone_pos:
+            raise ValueError('Unsatisfiable formula')
+        elif alone_pos:
+            result_clauses = [
+                [var for var in clause if var != -variable]
+                if index in neg
+                else clause
+                for index, clause in enumerate(clauses)
+                if index not in pos
+            ]
+        elif alone_neg:
+            result_clauses = [
+                [var for var in clause if var != variable]
+                if index in pos
+                else clause
+                for index, clause in enumerate(clauses)
+                if index not in neg
+            ]
+
+        new_formula = CNF(from_clauses=result_clauses)
+
+    if new_formula.clauses != formula.clauses:
+        return True, new_formula
+    else:
+        return False, new_formula
+
+
+def simplify(formula: CNF) -> CNF:
+    changed, new_formula = unit_propagation(formula)
+    while changed:
+        changed, new_formula = unit_propagation(new_formula)
+    return new_formula
+
+
+if __name__ == '__main__':
+    f = CNF(from_file='formula.cnf')
+    new_f = simplify(f)
+    print(new_f.clauses)
+    print(f.clauses == new_f.clauses)
