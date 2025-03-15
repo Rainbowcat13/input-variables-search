96bce13aef54393adcfa44792046f933fd743319
danya <d.golov@vkteam.ru>
2025-03-15 15:59:26 +0300

    Inputs extraction – replace .blif with .aig – add input extraction to conversion – replace berkeley-abc with yongshiwo-abc (though it is not needed anymore)

--- create_tests.py
+++ create_tests.py
@@ -2,32 +2,39 @@ import os
 import sys
 from pathlib import Path
 
+import aiger
+from aiger_cnf import aig2cnf
+from pysat.formula import CNF
+
+sys.setrecursionlimit(10 ** 9)
+
 output_dir = 'tests'
 if len(sys.argv) > 1:
     output_dir = sys.argv[1]
 
-blif_dirs = ['benchmarks/arithmetic', 'benchmarks/random_control']
-blif_files = [
+aig_dirs = ['benchmarks/arithmetic', 'benchmarks/random_control']
+aig_files = [
     str(file.absolute())
     for file in sum([
         list(Path(d).glob('**/*'))
-        for d in blif_dirs
+        for d in aig_dirs
     ], [])
-    if file.name.endswith('.blif')
+    if file.name.endswith('.aig')
 ]
 
-for blf in blif_files:
-    cnf_filename = f'{blf.split("/")[-1].split(".")[0]}.cnf'
-    map_filename = cnf_filename.replace('.cnf.', '.map')
+for aig in aig_files:
+    cnf_filename = f'{aig.split("/")[-1].split(".")[0]}.cnf'
+    inputs_filename = cnf_filename.replace('.cnf', '.inputs')
     if os.path.exists(f'{output_dir}/{cnf_filename}'):
         print(f'File {cnf_filename} already exists. To regenerate clear {output_dir}.')
     else:
-        print(f'\nConverting {blf}...')
-        os.system(
-            f'abc/abc -c "read_blif {blf};'
-            f' strash; '
-            f'write_cnf -map {output_dir}/{map_filename} {output_dir}/{cnf_filename}"'
-        )
+        print(f'\nConverting {aig}...')
+        schema = aiger.load(aig)
+        cnf = aig2cnf(schema)
+        inputs = list(sorted(cnf.input2lit.values()))
+        CNF(from_clauses=cnf.clauses).to_file(f'{output_dir}/{cnf_filename}')
+        with open(f'{output_dir}/{inputs_filename}', 'w') as inputs_file:
+            inputs_file.write(f'{len(inputs)}\n{" ".join(map(str, inputs))}\n')
 
 if os.path.exists('abc.history'):
     os.system('rm abc.history')
