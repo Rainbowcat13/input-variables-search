8ffd1dc55359caf924537961cc4c45330a0e9953
Rainbowcat13 <rainbowcat1308@gmail.com>
2025-03-31 18:16:04 +0300

    Modify orchestra to run tests – add simple argparser for orchestra – print solution – update run-test for orchestra

--- orchestra.py
+++ orchestra.py
@@ -1,6 +1,7 @@
 import random
 import sys
 from multiprocessing import Pool, freeze_support
+import argparse
 
 from pysat.formula import CNF
 from pysat.solvers import Glucose3
@@ -10,30 +11,39 @@ from evolution import evolution, evolution_arg_parser
 from util import fitness
 
 
+orchestra_arg_parser = argparse.ArgumentParser(
+    prog='Orchestra of evolution and greedy algorithms',
+    description='Uses orchestra of 2 algorithms to determine input variables of a logical schema in CNF'
+)
+orchestra_arg_parser.add_argument('filename', type=str, help='.cnf filename to get function from', default='formula.cnf')
+orchestra_arg_parser.add_argument('-d', '--debug-info', type=bool, help='show debug info', default=False)
+orchestra_arg_parser.add_argument('-r', '--random-seed', type=int, help='seed for random', default=1337)
+orchestra_arg_parser.add_argument('-m', '--metric-mode', type=str,
+                                  help='what metric to use to determine best result', choices=['prop', 'conflicts'],
+                                  default='prop')
+
+
 def create_evolution_params(set_size):
     return evolution_arg_parser.parse_args(
         [
             'pupa',
             '-s', str(set_size),
             '-e', '120',
-            '-g', '10000',
+            '-g', '12000',
             '-r', '13',
         ]
     )
 
 
-upper_bounds = sum([[2 ** x] * 2 for x in range(3, 9)], [])
+upper_bounds = sum([[2 ** x] for x in range(3, 9)], [])
 RANDOM_SAMPLES_COUNT = len(upper_bounds)
 
 if __name__ == '__main__':
     freeze_support()
-    formula_filename = 'formula.cnf'
-    if len(sys.argv) > 1:
-        formula_filename = sys.argv[1]
+    params = orchestra_arg_parser.parse_args()
+    random.seed(params.random_seed)
 
-    random.seed(13)
-
-    formula = CNF(from_file=formula_filename)
+    formula = CNF(from_file=params.filename)
     g = Glucose3(formula.clauses)
 
     expansion_pool_args = [
@@ -44,7 +54,6 @@ if __name__ == '__main__':
         )
         for var, upper_bound in zip(
             random.sample(range(1, formula.nv), RANDOM_SAMPLES_COUNT),
-            # list(range(1, formula.nv)),
             upper_bounds
         )
     ]
@@ -62,12 +71,13 @@ if __name__ == '__main__':
         for candidate in evolution_candidates
     ]
 
-    sys.stderr.write('Expansion stage results...\n')
-    for result in evolution_candidates:
-        prop_ratio, conflict_ratio = fitness(g, result, 1000)
-        print(f'Propagation ratio: {prop_ratio}. Conflict ratio: {conflict_ratio}')
-        print(len(result))
-        print(*sorted(result))
+    if params.debug_info:
+        print('Expansion stage results...')
+        for result in evolution_candidates:
+            prop_ratio, conflict_ratio = fitness(g, result, 1000)
+            print(f'Propagation ratio: {prop_ratio}. Conflict ratio: {conflict_ratio}')
+            print(len(result))
+            print(*sorted(result))
 
     sys.stderr.write('Starting evolution stage...\n')
     with Pool(RANDOM_SAMPLES_COUNT) as pool:
@@ -78,8 +88,28 @@ if __name__ == '__main__':
     # Возможно, бинпоиск?
 
     sys.stderr.write('Starting estimation stage...\n')
+
+    if params.debug_info:
+        for result in results:
+            prop_ratio, conflict_ratio = fitness(g, result[0], 1000)
+            print(f'Propagation ratio: {prop_ratio}. Conflict ratio: {conflict_ratio}')
+            print(len(result[0]))
+            print(*sorted(result[0]))
+
+    best_prop, best_conflicts = None, None
+    best_ratio_prop, best_ratio_conflicts = 0, 1.01
     for result in results:
-        prop_ratio, conflict_ratio = fitness(g, result[0], 1000)
-        print(f'Propagation ratio: {prop_ratio}. Conflict ratio: {conflict_ratio}')
-        print(len(result[0]))
-        print(*sorted(result[0]))
+        prop_ratio, conflict_ratio = fitness(g, result[0], 5000)
+        if prop_ratio > best_ratio_prop:
+            best_prop = result[0]
+            best_ratio_prop = prop_ratio
+        if conflict_ratio < best_ratio_conflicts:
+            best_conflicts = result[0]
+            best_ratio_conflicts = conflict_ratio
+
+    if params.metric_mode == 'prop':
+        print(len(best_prop))
+        print(*sorted(best_prop))
+    elif params.metric_mode == 'conflicts':
+        print(len(best_conflicts))
+        print(*sorted(best_conflicts))
--- run-test.sh
+++ run-test.sh
@@ -1,19 +1,22 @@
 mkdir answers
 
 solutions=(
-  "evolution.py"
+  "orchestra.py"
+#  "evolution.py"
 #  "fullscan_border.py"
 )
 
+mkdir answers/orchestra/prop
+mkdir answers/orchestra/conflicts
 for solution in "${solutions[@]}"; do
   solution_name="${solution%.*}"
   mkdir answers/"$solution_name"
   cnt=1
-  for test_filename in tests/*.cnf; do
+  for test_filename in tests/cnf/*.cnf; do
     test_name=$(basename -- "$test_filename")
     ans_name="${test_name%.*}"
     echo "Test $test_name on $solution_name $cnt"
-    python3 "$solution" "$test_filename" > answers/"$solution_name"/"$ans_name.ans"
+    python3 "$solution" "$test_filename" -m prop > answers/"$solution_name"/prop/"$ans_name.ans"
     cnt=$((cnt+1))
   done
 done
